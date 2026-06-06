f203b9b74b9ac337bfbaeffb279d9493da58b012
Icaro Gabriel <icarogabrielpc05@gmail.com>
2026-06-06 18:29:10 -0300

    feat: corrige indexacao do post no posts do perfil

--- frontend-android/picture_show/lib/features/perfil/pages/perfil_posts_page.dart
+++ frontend-android/picture_show/lib/features/perfil/pages/perfil_posts_page.dart
@@ -5,7 +5,7 @@ import 'package:picture_show/shared/widgets/headers/page_header.dart';
 import 'package:picture_show/features/post/widgets/post_card.dart';
 import 'package:provider/provider.dart';
 
-class PerfilPostsPage extends StatelessWidget{
+class PerfilPostsPage extends StatefulWidget {
 
   final Profile profile;
   final int initialIndex;
@@ -13,46 +13,63 @@ class PerfilPostsPage extends StatelessWidget{
   const PerfilPostsPage({
     super.key,
     required this.profile,
-    required this.initialIndex
+    required this.initialIndex,
   });
+
+  @override
+  State<PerfilPostsPage> createState() => _PerfilPostsPageState();
+}
+
+class _PerfilPostsPageState extends State<PerfilPostsPage> {
+
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      const estimatedPostHeight = 545.0;
+      _scrollController.jumpTo(widget.initialIndex * estimatedPostHeight);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     
     final postProvider = context.watch<PostProvider>();
-    final profilePosts = postProvider.getPostsByProfileId(profile.id);
+    final profilePosts = postProvider.getPostsByProfileId(widget.profile.id);
 
     return Scaffold(
       backgroundColor: const Color(0xFFFFFEEF),
 
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+        child: ListView.builder(
+          controller: _scrollController,
+          padding: const EdgeInsets.all(16),
+          itemCount: profilePosts.length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return const Column(
+                children: [
+                  PageHeader(nome: 'Posts'),
+                  SizedBox(height: 12),
+                  Divider(color: Color(0xFF3C3535), thickness: 1),
+                  SizedBox(height: 12),
+                ],
+              );
+            }
 
-                PageHeader(nome: "Posts"),
+            final post = profilePosts[index - 1];
 
-                const SizedBox(height: 12),
-
-                const Divider(
-                  color: Color(0xFF3C3535),
-                  thickness: 1,
-                ),
-
-                const SizedBox(height: 12),
-
-                ...profilePosts.map(
-                  (post) => FeedPostCard(
-                    post: post,
-                  ),
-                ),
-
-              ],
-            ),
-          ),
+            return FeedPostCard(post: post);
+          }
         ),
       ),
     );
